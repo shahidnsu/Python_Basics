f0370768017050dc52573e0732d473359d38c2ea
Shahidnsu <shahid.khan@northsouth.edu>
2020-02-24 01:11:39 +0600

    added two new filesand functions

---
+++ Turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.Screen() # creates a graphics window
+
+alex = turtle.Turtle() # creates a turtle named alex
+
+alex.forward(150)
+
+alex.left(90)
+alex.forward(75)
+
+alex.left(90)
+alex.backward(100)
---
+++ Unit_testing.py
@@ -0,0 +1,7 @@
+def square(x):
+    '''raisr x to the second power'''
+    return x * x 
+
+print(square(3))
+
+
